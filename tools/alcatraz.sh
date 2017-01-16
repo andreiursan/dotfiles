4bbb636ca5e8934d1de9384e8173ca99526f2975
Andrei Ursan <hello@andreiursan.com>
2017-01-16 19:29:47 +0100

    updated alcatraz

--- tools/alcatraz.sh
+++ tools/alcatraz.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Alcatraz http://alcatraz.io/
-curl -fsSL https://raw.github.com/supermarin/Alcatraz/master/Scripts/install.sh | sh
+curl -fsSL https://raw.githubusercontent.com/supermarin/Alcatraz/deploy/Scripts/install.sh | sh
